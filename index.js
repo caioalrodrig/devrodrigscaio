0b1e291f4436837850eb99a37c3d646e253eac30
caioalrodrig <caio.almeida@engenharia.ufjf.br>
2023-11-23 20:11:54 -0300

    first deploy

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+/* Botão de alteração das css variables */
 const toggle = document.getElementById("toggle");
-const theme = window.localStorage.getItem("theme");
-
-if (theme === "dark") document.body.classList.add("dark");
-
-// event listener para quando o botão de alterar o tema for clicado
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
   if (theme === "dark") {
     window.localStorage.setItem("theme", "light");
   } else window.localStorage.setItem("theme", "dark");
+});
+
+
+/*Ofuscamento da .navbar .fixed-top ao rolar com o scroll*/
+const navbar = document.querySelector('.navbar');
+
+document.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition > 100) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
 });
