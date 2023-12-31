5a8e5c6ec2b05dd8e8dcb1adf5cd2fe864dd0797
caioalrodrig <caio.almeida@engenharia.ufjf.br>
2023-12-30 22:11:38 -0300

    add carrousel (basic)

--- index.js
+++ index.js
@@ -37,6 +37,69 @@ document.addEventListener('DOMContentLoaded', () => {
   setTimeout(() => {
     hello.style.transform = "translateX(-40%)";
   }, 750);
-  
 
-})
+});
+
+// Select all slides
+const slides = document.querySelectorAll(".slide");
+
+// loop through slides and set each slides translateX property to index * 100% 
+slides.forEach((slide, indx) => {
+  slide.style.transform = `translateX(${indx * 100}%)`;
+});
+
+// current slide counter
+let curSlide = 0;
+
+// select next slide button
+const nextSlide = document.querySelector(".btn-next");
+
+// add event listener and next slide functionality
+nextSlide.addEventListener("click", function () {
+     curSlide++;
+
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${110 * (indx - curSlide)}%)`;
+  });
+});
+
+// select next slide button
+const nextSlide1 = document.querySelector(".btn-next");
+
+// current slide counter
+let curSlide1 = 0;
+// maximum number of slides
+let maxSlide = slides.length - 1;
+
+// add event listener and navigation functionality
+nextSlide1.addEventListener("click", function () {
+  // check if current slide is the last and reset current slide
+  if (curSlide1 === maxSlide) {
+    curSlide1 = 0;
+  } else {
+    curSlide1++;
+  }
+
+//   move slide by -100%
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${110 * (indx - curSlide1)}%)`;
+  });
+});
+
+const prevSlide = document.querySelector(".btn-prev");
+
+// add event listener and navigation functionality
+prevSlide.addEventListener("click", function () {
+  // check if current slide is the first and reset current slide to last
+  if (curSlide1 === 0) {
+    curSlide1 = maxSlide;
+  } else {
+    curSlide1--;
+  }
+
+  //   move slide by 100%
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${110 * (indx - curSlide1)}%)`;
+  });
+});
+
