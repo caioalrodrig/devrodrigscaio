a190ea0e51d041c51849b0e30a174d9d7c234303
caioalrodrig <caio.almeida@engenharia.ufjf.br>
2024-01-01 21:20:11 -0300

    toggle lang button added + icons update

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 /* Botão de alteração das css variables */
-const toggle = document.getElementById("toggle");
-const hello_img = document.getElementById("hello");
+const toggle = document.getElementById("toggle")
+const hello_img = document.getElementById("hello")
+const toggLang = document.getElementById("toggLang")
 
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
+  
 
   // Verifica se a classe "dark" está presente no body
   if (document.body.classList.contains("dark")) {
@@ -13,6 +15,38 @@ toggle.addEventListener("click", () => {
   }
 });
 
+toggLang.addEventListener("click", () => {
+  let name = document.getElementById('user-name')
+  let msg = document.getElementById('user-msg')
+  let email = document.getElementById('user-email')
+  let msgArea= document.getElementById('message')
+  let sendBtn = document.getElementById('btnSend-text')
+
+
+
+  if (toggLang.innerText === 'En'){
+    toggLang.innerText = 'Pt'
+    name.innerText = "Name:"
+    msg.innerText = "Message: "
+    email.innerHTML = "<em> Your </em> email address: "
+    msgArea.placeholder= "Tell me what is in your mind..."
+    sendBtn.innerHTML = "&nbsp;Send"
+  }else{
+    toggLang.innerText = 'En'
+    name.innerText = "Nome:"
+    msg.innerText = "Mensagem: "
+    email.innerHTML = "<em> Seu </em> endereço de e-mail: "
+    msgArea.placeholder= "Diga em quê posso ser útil..."
+    sendBtn.innerHTML = "&nbsp;Enviar"
+  }
+
+  // Verifica se a classe "english" está presente no body
+if (document.body.classList.contains("dark")) {
+    hello_img.src = 'utils/imgs/help-me.svg';
+  } else {
+    hello_img.src = 'utils/imgs/help-me-d.svg';
+  }
+});
 
 /*Ofuscamento da .navbar .fixed-top ao rolar com o scroll*/
 const navbar = document.querySelector('.navbar');
@@ -40,3 +74,4 @@ document.addEventListener('DOMContentLoaded', () => {
   
 
 })
+
